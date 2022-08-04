05326d56b15770c8051f9bd86036b7bd2311f69c
SamsonDeVol <devols@oregonstate.edu>
2022-08-04 10:50:36 -0700

    Add frequency visualiser and refactor into functions

--- first_webaudio_page/second_script.js
+++ first_webaudio_page/second_script.js
@@ -8,39 +8,86 @@ function get_decibel(x) {
   return 20 * log10(x)
 }
 
-// create audio context
+function graph_frequency(analyser){
+  analyser.fftSize = 256;
+  const bufferLength = analyser.frequencyBinCount;
+  console.log(bufferLength);
+  const dataArray = new Uint8Array(bufferLength);
+
+  var canvas = document.querySelector('.visualizer');
+  var canvasCtx = canvas.getContext("2d");
+
+  WIDTH = 640;
+  HEIGHT = 100;
+  canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+  // draw an oscilloscope of the current audio source
+
+  var draw = function() {
+    drawVisual = requestAnimationFrame(draw);
+
+    analyser.getByteFrequencyData(dataArray);
+
+    canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
+    document.getElementById("b").innerHTML = `${dataArray}`;
+    for(var i = 0; i < bufferLength; i++) {
+      barHeight = dataArray[i];
+
+      canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+      canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
+
+      x += barWidth + 1;
+    }
+  };
+
+  draw();
+}
+
+function play_button(audioCtx, audioElement){
+  const playButton = document.querySelector('button');
+  playButton.addEventListener('click', function() {
+      if (audioCtx.state === 'suspended') {
+          audioCtx.resume();
+      }
+      if (this.dataset.playing === 'false') {
+          audioElement.play();
+          this.dataset.playing = 'true';
+      } else if (this.dataset.playing === 'true') {
+          audioElement.pause();
+          this.dataset.playing = 'false';
+      }
+  }, false);
+
+    // add button listner
+  audioElement.addEventListener('ended', () => {
+    playButton.dataset.playing = 'false';
+  }, false);
+}
+
+function volume_control(audioCtx, gainNode,){
+  const volumeControl = document.querySelector('#volume');
+  volumeControl.addEventListener('input', function() {
+    if (audioCtx.state === 'suspended') {
+      audioCtx.resume();
+    }
+    else{
+      gainNode.gain.value = this.value;
+      document.getElementById("a").innerHTML = `dB:${get_decibel( gainNode.gain.value )}`;
+    }
+  }, false);
+}
+
 const audioContext = new AudioContext();
 const audioElement = document.querySelector('audio');
 const track = audioContext.createMediaElementSource(audioElement);
-track.connect(audioContext.destination);
-
-// play button
-const playButton = document.querySelector('button');
-playButton.addEventListener('click', function() {
-    if (audioContext.state === 'suspended') {
-        audioContext.resume();
-    }
-    if (this.dataset.playing === 'false') {
-        audioElement.play();
-        this.dataset.playing = 'true';
-    } else if (this.dataset.playing === 'true') {
-        audioElement.pause();
-        this.dataset.playing = 'false';
-    }
-}, false);
-
-// volume slider
 const gainNode = audioContext.createGain();
-track.connect(gainNode).connect(audioContext.destination);
-const volumeControl = document.querySelector('#volume');
-
-volumeControl.addEventListener('input', function() {
-  gainNode.gain.value = this.value;
-  document.getElementById("a").innerHTML = `dB:${get_decibel( gainNode.gain.value )}`;
-}, false);
-
-// add button listner
-audioElement.addEventListener('ended', () => {
-  playButton.dataset.playing = 'false';
-}, false);
-
+const analyser = audioContext.createAnalyser();
+track.connect(gainNode).connect(analyser).connect(audioContext.destination);
+graph_frequency(analyser);
+play_button(audioContext, audioElement);
+volume_control(audioContext, gainNode);
