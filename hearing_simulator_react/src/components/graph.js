f0f21717b3002254cde1170e3f1e7cc5ee6fd856
SamsonDeVol <devols@oregonstate.edu>
2023-02-27 16:43:49 -0800

    small refactoring

--- hearing_simulator_react/src/components/graph.js
+++ hearing_simulator_react/src/components/graph.js
@@ -5,7 +5,7 @@ const Canvas = props => {
   const { draw, ...rest } = props
   const canvasRef = useCanvas(draw)
   
-  return <canvas className="canvas" ref={canvasRef} {...rest}/>
+  return <canvas className="canvas" width="640" height="400" ref={canvasRef} {...rest}/>
 }
 
 const useCanvas = draw => {
@@ -35,22 +35,22 @@ const useCanvas = draw => {
 }
 
 function GraphFrequencies(props){
-  console.log("neeed: ", props.analyser)
+  // console.log("neeed: ", props.analyser)
   // 4800Hz and 2048 fft = 2400/1024 = 23.4Hz per bin
   props.analyser.fftSize = 2048;
-
+ 
   const bufferLength = props.analyser.frequencyBinCount;
   // console.log(props.analyser.minDecibels);
   const dataArray = new Uint8Array(bufferLength);
 
 
-  const WIDTH = 1280;
-  const HEIGHT = 200;
+  const WIDTH = 640;
+  const HEIGHT = 400;
 
   // draw an oscilloscope of the current audio source
   const draw = (canvasContext, frameCount) => {
     // let drawVisual = requestAnimationFrame(draw);
-
+  
     props.analyser.getByteFrequencyData(dataArray);
     canvasContext.fillStyle = 'rgb(0, 0, 0)';
     canvasContext.fillRect(0, 0, WIDTH, HEIGHT);
@@ -59,8 +59,8 @@ function GraphFrequencies(props){
     var barHeight;
     var x = 0;
     for(var i = 0; i < bufferLength; i++) {
-      barHeight = dataArray[i];
-
+      barHeight = dataArray[i]*2;
+      
       // 250//23.4 = 10.68 for audiogram relation
       if(i < 10){
         canvasContext.fillStyle = 'rgb(255,0,0)';
--- hearing_simulator_react/src/index.js
+++ hearing_simulator_react/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 import Slider from './components/filter.js';
 import GraphFrequencies from './components/graph';
-// npm install react-slider
 
 // instantiate Web Audio API
 let AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -12,6 +11,8 @@ let audioElement = document.querySelector('audio');
 let track = audioContext.createMediaElementSource(audioElement);
 let gain = audioContext.createGain();
 let analyser = audioContext.createAnalyser();
+
+// create frequency filters
 let frequencies = [0, 250, 500, 1000, 2000, 4000, 8000]
 
 let filters = frequencies.map(frequency => {
@@ -21,12 +22,7 @@ let filters = frequencies.map(frequency => {
   return(filter)
 })
 
-console.log(filters)
-// let filter = audioContext.createBiquadFilter();
-// filter.type = "peaking"
-// filter.frequency.value = 50
 track.connect(filters[0]).connect(filters[1]).connect(filters[2]).connect(filters[3]).connect(filters[4]).connect(filters[5]).connect(filters[6]).connect(analyser).connect(gain).connect(audioContext.destination);
-
 
 function App() {
   const [filtersSettings0, setFilterSettings0] = useState({
@@ -53,37 +49,30 @@ function App() {
  
   
   const changeFilter0 = gainValue => {
-    console.log("la", gainValue)
     setFilterSettings0({...filtersSettings0, gain: gainValue})
     filters[0].gain.value = gainValue
   }
   const changeFilter1 = gainValue => {
-    console.log("la", gainValue)
     setFilterSettings1({...filtersSettings1, gain: gainValue})
     filters[1].gain.value = gainValue
   }
   const changeFilter2 = gainValue => {
-    console.log("la", gainValue)
     setFilterSettings2({...filtersSettings2, gain: gainValue})
     filters[2].gain.value = gainValue
   }
   const changeFilter3 = gainValue => {
-    console.log("la", gainValue)
     setFilterSettings3({...filtersSettings3, gain: gainValue})
     filters[3].gain.value = gainValue
   }
   const changeFilter4 = gainValue => {
-    console.log("la", gainValue)
     setFilterSettings4({...filtersSettings4, gain: gainValue})
     filters[4].gain.value = gainValue
   }
   const changeFilter5 = gainValue => {
-    console.log("la", gainValue)
     setFilterSettings5({...filtersSettings5, gain: gainValue})
     filters[5].gain.value = gainValue
   }
   const changeFilter6 = gainValue => {
-    console.log("la", gainValue)
     setFilterSettings6({...filtersSettings6, gain: gainValue})
     filters[6].gain.value = gainValue
   }
@@ -176,107 +165,3 @@ class WebAudio extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
-
-
-// Samson DeVol, Hearing Simulator usin Web Audio API in JavaScript
-// example from https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API#loading_sound
-
-// function log10(x) {
-//   return Math.log(x)/Math.LN10;
-// }
-
-// function toDecibel(x) {
-//   return 20 * log10(x)
-// }
-
-
-
-// function playButton(audioContext, audioElement){
-//   const playButton = document.querySelector('button');
-//   playButton.addEventListener('click', function() {
-//       if (audioContext.state === 'suspended') {
-//           audioContext.resume();
-//       }
-//       if (this.dataset.playing === 'false') {
-//           audioElement.play();
-//           this.dataset.playing = 'true';
-//       } else if (this.dataset.playing === 'true') {
-//           audioElement.pause();
-//           this.dataset.playing = 'false';
-//       }
-//   }, false);
-
-//     // add button listner
-//   audioElement.addEventListener('ended', () => {
-//     playButton.dataset.playing = 'false';
-//   }, false);
-// }
-
-// function volumeControl(audioContext, gain,){
-//   const volumeControl = document.querySelector('#volume');
-//   volumeControl.addEventListener('input', function() {
-//     if (audioContext.state === 'suspended') {
-//       audioContext.resume();
-//     }
-//     else{
-//       gain.gain.value = this.value;
-//       document.getElementById("a").innerHTML = `dB:${toDecibel( gain.gain.value )}`;
-//     }
-//   }, false);
-// }
-
-// function filterControl(audioContext, biquadFilter, sliderIndex, frequencyValue){
-//   const frequency_slider = document.querySelector(sliderIndex);
-//   frequency_slider.addEventListener('input', function() {
-//     if (audioContext.state === 'suspended'){
-//       audioContext.resume();
-//     }
-//     else{
-//       biquadFilter.type = "peaking";
-//       // center of the frequency band
-//       biquadFilter.frequency.value = frequencyValue;
-//       // sets slider Q factor, determining inverse band range
-//       // Q = frequency/BW (Band Width)
-//       biquadFilter.Q = 1
-//       // gain (or attenuation if negative) for dB level
-//       biquadFilter.gain.value = -(this.value)
-//       //document.getElementById("b").innerHTML = `frequency:${toDecibel( biquadFilter.frequency.value )}`;
-//     }
-//   }, false);
-// }
-
-// const audioContext = new AudioContext();
-// // console.log(audioContext.sampleRate);
-// // console.log(audioContext)
-// const audioElement = document.querySelector('audio');
-// const track = audioContext.createMediaElementSource(audioElement);
-// console.log(track)
-// const gain = audioContext.createGain();
-// const analyser = audioContext.createAnalyser();
-
-// // initialize filters
-// const biquadFilter0 = audioContext.createBiquadFilter();
-// const biquadFilter1 = audioContext.createBiquadFilter();
-// const biquadFilter2 = audioContext.createBiquadFilter();
-// const biquadFilter3 = audioContext.createBiquadFilter();
-// const biquadFilter4 = audioContext.createBiquadFilter();
-// const biquadFilter5 = audioContext.createBiquadFilter();
-// const biquadFilter6 = audioContext.createBiquadFilter();
-
-// // connect audio elements
-// track.connect(biquadFilter0).connect(biquadFilter1).connect(biquadFilter2).connect(biquadFilter3).connect(biquadFilter4).connect(biquadFilter5).connect(biquadFilter6).connect(analyser).connect(gain).connect(audioContext.destination);
-
-// // connect interfaces 
-// graphFrequency(analyser);
-// playButton(audioContext, audioElement);
-// volumeControl(audioContext, gain);
-
-// // pass biquad filters to specific function
-
-// filterControl(audioContext, biquadFilter0, '#frequency_0', 0);
-// filterControl(audioContext, biquadFilter1, '#frequency_1', 250);
-// filterControl(audioContext, biquadFilter2, '#frequency_2', 500);
-// filterControl(audioContext, biquadFilter3, '#frequency_3', 1000);
-// filterControl(audioContext, biquadFilter4, '#frequency_4', 2000);
-// filterControl(audioContext, biquadFilter5, '#frequency_5', 4000);
-// filterControl(audioContext, biquadFilter6, '#frequency_6', 8000);
